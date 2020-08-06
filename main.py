d3a9a7d734cdd13eeb7c4788e8e9892ff19e30d6
Alan Mao <pebblealan@gmail.com>
2020-08-06 07:36:00 +0000

    folder save system

--- main.py
+++ main.py
@@ -1,42 +1,83 @@
 import requests
+import os
+import tempfile
+import csv
 
-genes = ['PF3D7_0102900', 'PF3D7_1218800', 'PF3D7_0411200', 'PF3D7_0419900', 'PF3D7_0526600', 'PF3D7_0604600', 'PF3D7_0608900', 'PF3D7_0826000', 'PF3D7_1414700']
+'''
+TODO:
+
+1. stitch together data files into one text file that can be pasted into excel
+2. fasta if possible only
+3. delete the data files after finished stitching
+4. fix that print thing with the mod to cover 3 outcomes
+5. make sure to delete things
+
+'''
+
+input_string = input("Enter a list of geneIDs separated by spaces ")
+
+print("\n")
+genes = input_string.split()
+print("your gene library is ", genes, "\n")
+
+def saveFolder(gene, data, url, mod):
+  if mod is not "data":
+    if gene not in os.listdir():
+        os.mkdir(gene)    
+    #the mod will be like "data" or "gint" or "prot"
+    f = open(gene + '/{}'.format(mod) + gene + '.out', 'w')
+    f.write(requests.post(url, data = data).text)
+    print('recorded ' + gene + ' {} sequence'.format("protein" if mod == "prot" else "with/without intron"))
+    f.close()
+  else:
+    if gene not in os.listdir():
+      os.mkdir(gene)
+    os.mkdir(gene + '/data')
+    f = open(gene + "/data/{}".format(mod)+gene+'.out', 'w')
+    f.write(requests.post(url, data = data).text)
+    print('recorded ' + gene + ' {} sequence'.format("data"))
+    f.close()
+
 
 for gene in genes:
     url = 'https://plasmodb.org/plasmo/service/answer/report'
 
-    data = {'data': '{"answerSpec":{"questionName":"__GeneRecordClasses.GeneRecordClass__singleRecordQuestion__","parameters":{"primaryKeys":"PF3D7_0419900,PlasmoDB"}},"formatting":{"format":"fullRecord","formatConfig":{"attributes":["primary_key","chromosome"],"tables":["Alias","GeneLocation","OrthologsLite","pfal3D7_phenotype_pB_mutagenesis_MIS_MFS_RSRC","SignalP","TMHMM","GOTerms","Notes","ProteinProperties"],"includeEmptyTables":true,"attachmentType":"plain"}}}'}
-        
-    with open('data' + gene + '.out', 'w') as f:
-        f.write(requests.post(url, data = data).text)
-        print('recorded ' + gene + ' report')
+    data = {
+        'data':
+        '{"answerSpec":{"questionName":"__GeneRecordClasses.GeneRecordClass__singleRecordQuestion__","parameters":{"primaryKeys":"PF3D7_0419900,PlasmoDB"}},"formatting":{"format":"fullRecord","formatConfig":{"attributes":["primary_key","chromosome"],"tables":["Alias","GeneLocation","OrthologsLite","pfal3D7_phenotype_pB_mutagenesis_MIS_MFS_RSRC","SignalP","TMHMM","GOTerms","Notes","ProteinProperties"],"includeEmptyTables":true,"attachmentType":"plain"}}}'
+    }
+
+    
+    saveFolder(gene, data, url, "data")
 
 for gene in genes:
     url = 'https://plasmodb.org/plasmo/service/answer/report'
 
-    data = {'data': '{"answerSpec":{"questionName":"__GeneRecordClasses.GeneRecordClass__singleRecordQuestion__","parameters":{"primaryKeys":"PF3D7_0419900,PlasmoDB"}},"formatting":{"format":"srt","formatConfig":{"attachmentType":"plain","endOffset3":0,"type":"genomic","sourceIdFilter":"genesOnly","upstreamAnchor":"Start","upstreamSign":"plus","upstreamOffset":0,"downstreamAnchor":"End","downstreamSign":"plus","downstreamOffset":0,"onlyIdDefLine":"0","noLineBreaks":"0","startAnchor3":"Start","startOffset3":0,"endAnchor3":"End"}}}'}
+    data = {
+        'data':
+        '{"answerSpec":{"questionName":"__GeneRecordClasses.GeneRecordClass__singleRecordQuestion__","parameters":{"primaryKeys":"PF3D7_0419900,PlasmoDB"}},"formatting":{"format":"srt","formatConfig":{"attachmentType":"plain","endOffset3":0,"type":"genomic","sourceIdFilter":"genesOnly","upstreamAnchor":"Start","upstreamSign":"plus","upstreamOffset":0,"downstreamAnchor":"End","downstreamSign":"plus","downstreamOffset":0,"onlyIdDefLine":"0","noLineBreaks":"0","startAnchor3":"Start","startOffset3":0,"endAnchor3":"End"}}}'
+    }
 
-    with open('gint' + gene + '.out', 'w') as f:
-        f.write(requests.post(url, data = data).text)
-        print('recorded ' + gene + ' genomic sequence with introns')
-
-for gene in genes:
-    url = 'https://plasmodb.org/plasmo/service/answer/report'
-
-    data = {'data': '{"answerSpec":{"questionName":"__GeneRecordClasses.GeneRecordClass__singleRecordQuestion__","parameters":{"primaryKeys":"PF3D7_0419900,PlasmoDB"}},"formatting":{"format":"srt","formatConfig":{"attachmentType":"plain","endOffset3":0,"type":"CDS","sourceIdFilter":"genesOnly","upstreamAnchor":"Start","upstreamSign":"plus","upstreamOffset":0,"downstreamAnchor":"End","downstreamSign":"plus","downstreamOffset":0,"onlyIdDefLine":"0","noLineBreaks":"0","startAnchor3":"Start","startOffset3":0,"endAnchor3":"End"}}}'}
-
-    with open('gseq' + gene + '.out', 'w') as f:
-        f.write(requests.post(url, data = data).text)
-        print('recorded ' + gene + ' genomic sequence without introns')
-
+    saveFolder(gene, data, url, "gint")
 
 for gene in genes:
     url = 'https://plasmodb.org/plasmo/service/answer/report'
 
-    data = {'data': '{"answerSpec":{"questionName":"__GeneRecordClasses.GeneRecordClass__singleRecordQuestion__","parameters":{"primaryKeys":"PF3D7_0419900,PlasmoDB"}},"formatting":{"format":"srt","formatConfig":{"attachmentType":"plain","endOffset3":0,"type":"protein","sourceIdFilter":"genesOnly","upstreamAnchor":"Start","upstreamSign":"plus","upstreamOffset":0,"downstreamAnchor":"End","downstreamSign":"plus","downstreamOffset":0,"onlyIdDefLine":"0","noLineBreaks":"0","startAnchor3":"Start","startOffset3":0,"endAnchor3":"End"}}}'}
+    data = {
+        'data':
+        '{"answerSpec":{"questionName":"__GeneRecordClasses.GeneRecordClass__singleRecordQuestion__","parameters":{"primaryKeys":"PF3D7_0419900,PlasmoDB"}},"formatting":{"format":"srt","formatConfig":{"attachmentType":"plain","endOffset3":0,"type":"CDS","sourceIdFilter":"genesOnly","upstreamAnchor":"Start","upstreamSign":"plus","upstreamOffset":0,"downstreamAnchor":"End","downstreamSign":"plus","downstreamOffset":0,"onlyIdDefLine":"0","noLineBreaks":"0","startAnchor3":"Start","startOffset3":0,"endAnchor3":"End"}}}'
+    }
 
-    with open('prot' + gene + '.out', 'w') as f:
-        f.write(requests.post(url, data = data).text)
-        print('recorded ' + gene + ' protein sequence')
+    saveFolder(gene, data, url, "gseq")
+
+for gene in genes:
+    url = 'https://plasmodb.org/plasmo/service/answer/report'
+
+    data = {
+        'data':
+        '{"answerSpec":{"questionName":"__GeneRecordClasses.GeneRecordClass__singleRecordQuestion__","parameters":{"primaryKeys":"PF3D7_0419900,PlasmoDB"}},"formatting":{"format":"srt","formatConfig":{"attachmentType":"plain","endOffset3":0,"type":"protein","sourceIdFilter":"genesOnly","upstreamAnchor":"Start","upstreamSign":"plus","upstreamOffset":0,"downstreamAnchor":"End","downstreamSign":"plus","downstreamOffset":0,"onlyIdDefLine":"0","noLineBreaks":"0","startAnchor3":"Start","startOffset3":0,"endAnchor3":"End"}}}'
+    }
+
+    saveFolder(gene, data, url, "prot")
 
 print('query complete')
